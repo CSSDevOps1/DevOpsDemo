b10ce6030d8a38ac62f73225db68c03610022eb3
mspalanimanickam <mspalanimanickam@HDC0007527>
2017-03-30 11:54:28 +0530

    Add project files.

---
+++ SampleBoilerTemp.Web/App/Main/views/RequestForm/RequestForm.js
@@ -0,0 +1,306 @@
+﻿
+var app = angular.module('app');
+app.controller("app.views.RequestForm", function ($scope, $timeout, requestAJService, $window, appSession, $location) {
+    $scope.requests = [];
+    $scope.emp = null;
+   
+    $scope.From = appSession.user.name;
+    var today = new Date();
+    var date = today.getDate() + "-" + today.toLocaleString("en-us", { month: "long" }) + "-" + today.getFullYear();
+    //var month = today.toLocaleString("en-us", { month: "long" });
+    $scope.Date = today;
+    $scope.Month = today.toLocaleString("en-us", { month: "long" });;
+
+    GetAllDetails();
+    Bind();
+    function BindData() {
+
+        $scope.requests.push($scope.emp);
+
+
+    }
+    function  Bind()
+
+    {
+        for (var i = 0; i <8; i++) {
+            $scope.showEdit = false;
+            var index = 0;
+            if ($scope.requests.length != 0)
+                index = $scope.requests.length;
+
+            var emp = {
+                Index: index,
+
+                UserId: appSession.user.id,
+                TenantId: appSession.tenant.id
+            };
+
+
+            $scope.emp = emp;
+
+            BindData();
+        }
+    }
+
+    function GetAllDetails()
+    {
+        GetParticulars();
+        GetUnits();
+        var qs = $location.search();
+        if (qs.id != null) {
+            var getParticularData = requestAJService.getDetails(qs.id);
+
+            getParticularData.success(function (particular) {
+                debugger;
+
+
+                $scope.requests = particular;
+            }, function () {
+                debugger;
+                alert('Error in getting particular records');
+            });
+        }
+    }
+   
+  
+  function GetParticulars() {
+        debugger;
+        var getParticularData = requestAJService.getAllParticulars();
+
+        getParticularData.success(function (particular) {
+            debugger;
+            $scope.particulars = particular;
+        }, function () {
+            debugger;
+            alert('Error in getting particular records');
+        });
+    }
+  $scope.SubmitRequestDiv = function ()
+    {
+            
+      $scope.showEdit = true;
+        var req = requestAJService.addRequest($scope.requests,appSession.tenant.id, appSession.user.id);
+        req.then(function (msg) {
+            $window.location.href = '#/test';
+            //request.showEdit = request.showEdit ? false : true;
+            //$scope.Result = "Request successfully submitted";
+        
+        }, function () {
+            alert('Error in updating emp record');
+        });
+      
+
+    }
+    function GetUnits() {
+        var getUnits = requestAJService.getUnits();
+
+        getUnits.success(function (unit) {
+            $scope.units = unit;
+        }, function () {
+            alert('Error in getting unit records');
+        });
+    }
+
+    
+
+    
+
+    $scope.EditDetails = function () {
+        $scope.showEdit = false;
+        $scope.ParticularId = request.ParticularId;
+        $scope.UnitId = request.UnitId;
+        $scope.Rate = request.Rate;
+        $scope.Vat = request.Vat;
+        $scope.VateRate = request.VateRate;
+        $scope.FinalRate = request.FinalRate;
+        $scope.Quantity = request.Quantity;
+        $scope.Cost = request.Cost;
+        $scope.Action = "Update";
+        $scope.updateIndex = request.Id;
+        $scope.divAddRequest = true;
+    }
+
+    $scope.AddRequest = function () {
+        $scope.showEdit =  false;
+        var index = 0;
+        if ($scope.requests.length != 0)
+             index = $scope.requests.length;
+           
+        var emp = {
+            Index: index,
+           
+            UserId: appSession.user.id,
+            TenantId: appSession.tenant.id
+    };
+       
+        var getEmpAction = $scope.Action;
+        $scope.emp = emp;
+        if ($scope.Action == "Update")
+            $scope.emp.Index = $scope.updateIndex;
+            BindData();
+
+    }
+    $scope.GetRatedetails = function (request) {
+
+        if (request.ParticularDesc != null && request.UnitDesc != null) {
+            var req = requestAJService.getRateDetails(request.ParticularDesc, request.UnitDesc);
+            req.then(function (msg) {
+                request.Rate = msg.data[0].Rate;
+                request.Vat = msg.data[0].VAT;
+                request.VateRate = request.Vat * request.Rate / 100;
+                request.FinalRate = request.Rate + request.VateRate;
+                request.Cost = request.FinalRate * request.Quantity;
+            }, function () {
+                alert('Error in updating emp record');
+            });
+        }
+    }
+
+    $scope.AddRequestDiv = function () {
+        ClearFields();
+        $scope.Action = "Add";
+        $scope.divAddRequest = true;
+        GetParticulars();
+        GetUnits();
+    }
+
+    $scope.DeleteRequest=function() {
+    
+        $scope.requests = [];
+      
+    }
+
+    
+    $scope.FinalCost = function (request)
+    {
+        request.Cost = request.FinalRate * request.Quantity;
+
+    }
+
+    function ClearFields() {
+        $scope.ParticularId = "";
+        $scope.UnitId ="";
+        $scope.Rate = "";
+        $scope.Vat = "";
+        $scope.VateRate = "";
+        $scope.FinalRate = "";
+        $scope.Quantity = "";
+        $scope.Cost = "";
+        GetParticulars();
+        GetUnits();
+    }
+    $scope.Cancel = function () {
+      
+        
+    };
+    $scope.RemoveDetails = function (request) {
+
+        for (var i = 0; i < $scope.requests.length; i++) {
+            if ($scope.requests[i].Index == request.Index) {
+              
+                    $scope.requests.splice(i, 1);
+             
+            }
+
+        }
+    }
+
+    $scope.toggleEdit = function (request) {
+        request.showEdit = request.showEdit ? false : true;
+    }
+});
+
+app.service("requestAJService", function ($http, $q) {
+    
+    this.getAllParticulars = function () {
+        var response = $http({
+            method: "get",
+            url: "RequestForm/GetAllParticulars",
+            dataType: "json"
+        });
+        return response;
+    }
+
+    this.getDetails = function (id) {
+        var response = $http({
+            method: "post",
+            url: "RequestForm/EditRequest",
+            params: {
+                requestId: id
+            }
+            
+        });
+        return response;
+    }
+
+    this.getUnits = function () {
+        var response = $http({
+            method: "get",
+            url: "RequestForm/GetUnits",
+            dataType: "json"
+        });
+        return response;
+    }
+
+    this.getRateDetails = function (particulardesc,untidesc) {
+        var response = $http({
+            method: "post",
+            url: "RequestForm/GetRateDetails",
+            params: {
+                particular: JSON.stringify(particulardesc),
+                unit: JSON.stringify(untidesc)
+
+            }
+        });
+        return response;
+    }
+
+    this.updateEmployee = function (emp) {
+        var response = $http({
+            method: "post",
+            url: "Employee/UpdateEmployee",
+            data: JSON.stringify(emp),
+            dataType: "json"
+        });
+        return response;
+    }
+
+    this.addRequest = function (emp, tenant, user) {
+        emp.tenant = tenant;
+        var response = $http({
+            method: "post",
+            //url: "Employee/AddEmployee",
+            url: "RequestForm/PostStationaryRequestDetails",
+            data: JSON.stringify(emp),
+          
+            dataType: "json"
+        });
+        return response;
+    }
+
+    this.deleteEmployee = function (employeeId) {
+        var response = $http({
+            method: "post",
+            url: "Employee/DeleteEmployee",
+            params: {
+                empId: JSON.stringify(employeeId)
+            }
+        });
+        return response;
+    }
+
+
+});
+
+app.config(['$httpProvider', function ($httpProvider) {
+    //initialize get if not there
+    if (!$httpProvider.defaults.headers.get) {
+        $httpProvider.defaults.headers.get = {};
+    }
+    //disable IE ajax request caching
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+}]);
+
+
---
+++ SampleBoilerTemp.Web/Controllers/RequestFormController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SampleBoilerTemp.Web.Models.DAO;
+using System.Web.Http.Description;
+using System.Threading.Tasks;
+using SampleBoilerTemp.Web.Models.Account;
+using System.Web.Mvc;
+
+namespace SampleBoilerTemp.Web.App_Start
+{
+    public class RequestFormController : Controller
+    {
+        private CoreRequestEntities smsEntity = new CoreRequestEntities();
+
+        public ActionResult GetAllRequests(int tenantId,int userId)
+        {
+            var values = from request in smsEntity.StationaryRequests
+                         join st in smsEntity.Status on request.StatusId equals st.StatusId
+                         join user in smsEntity.AbpUsers on request.UserId equals user.Id where user.TenantId==tenantId && user.Id==userId
+                         select new { request.RequestId, st.StatusDescription, user.UserName, request.CreatedDate };
+
+            return Json(values.ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetAllParticulars()
+        {
+            var values = from particular in smsEntity.SMS_Particulars select new { particular.Item_id,particular.Item_Description};
+            return Json(values.ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetUnits()
+        {
+            var values = from unit in smsEntity.SMS_Units select new { unit.Unit_Id, unit.Unit_Description };
+            return Json(values.ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult EditRequest(int? requestId)
+        {
+            var requestdetails = from req in smsEntity.StationaryRequestDetails join  part in smsEntity.SMS_Particulars on req.ParticularId equals part.Item_id join 
+                                unit in smsEntity.SMS_Units on req.UnitId equals unit.Unit_Id join reqst in smsEntity.StationaryRequests on req.RequestId equals reqst.RequestId  where req.RequestId == requestId 
+                                 select new {part.Item_Description,req.Quantity,req.Rate,req.Id,req.VateRate,unit.Unit_Description,req.Cost,reqst.TenantId,reqst.UserId,req.RequestId };
+            return Json(requestdetails.ToList(), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult GetRateDetails(string particular,string unit)
+        {
+            particular = particular.Trim('"');
+            unit = unit.Trim('"');
+            var particularId = (from part in smsEntity.SMS_Particulars where part.Item_Description == particular select part.Item_id).FirstOrDefault();
+            var UnitId = (from units in smsEntity.SMS_Units where units.Unit_Description == unit select units.Unit_Id).FirstOrDefault();
+
+            var rate = from rates in smsEntity.SMS_Particular_Unit where rates.Item_id == particularId && rates.Unit_Id == UnitId select new { rates.VAT, rates.Rate };
+            
+            return Json(rate.ToList(), JsonRequestBehavior.AllowGet);
+        }
+        public string PostStationaryRequestDetails(List<Stationary_Request> stRequest)
+        {
+            if (stRequest[0].RequestId == null)
+            {
+                long userid = stRequest[0].UserId;
+                int? tenantid = stRequest[0].TenantId;
+                var username = (from user in smsEntity.AbpUsers where user.Id == userid && user.TenantId == tenantid select user.UserName).FirstOrDefault();
+
+                //var user = smsEntity.AbpUsers.Select(x => x.Id == stRequest[0].UserId).SingleOrDefault();
+                // var userName = smsEntity.AbpUsers.Where(a => a.Id == stRequest[0].UserId).Select(x => x.UserName).SingleOrDefault();
+                //var userName = b.Countries.First(a => a.DOTWInternalID == citee.CountryCode).ID from test in smsEntity.AbpUsers where test.Id == stRequest.UserId && test.TenantId == stRequest.TenantId select test.UserName.FirstOrDefault();
+                StationaryRequest stationaryRequest = new StationaryRequest();
+                stationaryRequest.UserId = stRequest[0].UserId;
+                stationaryRequest.TenantId = stRequest[0].TenantId;
+                stationaryRequest.CreatedBy = username;//userName.ToString();
+                stationaryRequest.StatusId = 1;
+                stationaryRequest.IsActive = 1;
+                stationaryRequest.CreatedDate = DateTime.Now;
+                smsEntity.StationaryRequests.Add(stationaryRequest);
+                smsEntity.SaveChanges();
+
+                //var reqId = from test in smsEntity.StationaryRequests where test.UserId == stRequest.UserId && test.TenantId == stRequest.TenantId && test.RequestId == stationaryRequest.RequestId select test.RequestId.va
+                //var reqId = smsEntity.StationaryRequests.First(a => a.UserId == stRequest.UserId && a.TenantId == stRequest.TenantId).RequestId;
+                var reqIds = (from req in smsEntity.StationaryRequests where req.UserId == userid && req.TenantId == tenantid orderby req.RequestId descending select req.RequestId).FirstOrDefault();
+                //var reqId = smsEntity.StationaryRequests.Where(a => a.UserId == stRequest[0].UserId && a.TenantId == stRequest[0].TenantId)
+                //      .OrderByDescending(a => a.RequestId)
+                //   .Select(a => a.RequestId).FirstOrDefault();
+
+
+
+                foreach (Stationary_Request obj in stRequest)
+                {
+                    if (obj.ParticularDesc != null && obj.UnitDesc != null)
+                    {
+                        var particularId = (from part in smsEntity.SMS_Particulars where part.Item_Description == obj.ParticularDesc select part.Item_id).FirstOrDefault();
+                        var UnitId = (from unit in smsEntity.SMS_Units where unit.Unit_Description == obj.UnitDesc select unit.Unit_Id).FirstOrDefault();
+                        StationaryRequestDetail stationaryRequestDetail = new StationaryRequestDetail();
+                        stationaryRequestDetail.ParticularId = particularId;
+                        stationaryRequestDetail.RequestId = reqIds;
+                        stationaryRequestDetail.UnitId = UnitId;
+                        stationaryRequestDetail.Rate = obj.Rate;
+                        stationaryRequestDetail.Vat = obj.Vat;
+                        stationaryRequestDetail.VateRate = obj.VateRate;
+                        stationaryRequestDetail.FinalRate = obj.FinalRate;
+                        stationaryRequestDetail.Quantity = obj.Quantity;
+                        stationaryRequestDetail.Cost = obj.Cost;
+                        smsEntity.StationaryRequestDetails.Add(stationaryRequestDetail);
+                        smsEntity.SaveChanges();
+                    }
+
+                }
+            }
+
+
+            return "";
+        }
+        public void Add()
+        {
+            //var val = smsEntity.AbpUsers.Select(x => x.Id == 1 && x.TenantId == 1).FirstOrDefault();
+            var userName = from test in smsEntity.AbpUsers where test.Id == 1 select test.UserName.FirstOrDefault();
+            StationaryRequest obj = new StationaryRequest();
+            obj.UserId = 1;
+            obj.TenantId = 1;
+            obj.CreatedBy = userName.ToString();
+
+            smsEntity.StationaryRequests.Add(obj);
+            smsEntity.SaveChanges();
+
+        }
+
+
+    }
+}
